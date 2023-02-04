665dd67bb2b88d190801e97769b91e962535db89
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-04 17:36:09 -0500

    Set up flask_login

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy_utils import URLType
-
+from flask_login import UserMixin
 from grocery_app.extensions import db
 from grocery_app.utils import FormEnum
 
@@ -34,7 +34,7 @@ class GroceryItem(db.Model):
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_by = db.relationship('User')    
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     """User model"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
